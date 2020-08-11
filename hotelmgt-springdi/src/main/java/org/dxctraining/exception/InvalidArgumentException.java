d42a97dbd63452b2c03f5af56d7f06d54d868c26
amalpradeep-mv <amalpradeepmv@gmail.com>
2020-08-12 00:19:29 +0530

    update

--- hotelmgt-springdi/src/main/java/org/dxctraining/exception/InvalidArgumentException.java
+++
@@ -1,8 +0,0 @@
-package org.dxctraining.exception;
-
-public class InvalidArgumentException extends RuntimeException{
-
-		public InvalidArgumentException(String msg) {
-			super(msg);
-		}
-}
--- hotelmgt/src/main/java/org/dxctraining/exception/GuestNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package org.dxctraining.exception;
-
-public class GuestNotFoundException extends RuntimeException{
-
-		public GuestNotFoundException(String msg) {
-			super(msg);
-		}
-}
