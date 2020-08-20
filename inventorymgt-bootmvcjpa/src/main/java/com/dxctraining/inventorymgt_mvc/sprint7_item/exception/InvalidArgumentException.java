04c1056a8e41768f2dc9601f424339c5390934cf
amalpradeep-mv <amalpradeepmv@gmail.com>
2020-08-20 11:50:13 +0530

    update

--- inventorymgt-bootmvcjpa/src/main/java/com/dxctraining/inventorymgt_mvc/sprint7_item/exception/InvalidArgumentException.java
+++
@@ -1,8 +0,0 @@
-package com.dxctraining.inventorymgt_mvc.sprint7_item.exception;
-
-public class InvalidArgumentException extends RuntimeException {
-
-	public InvalidArgumentException(String msg) {
-		super(msg);
-	}
-}
--- inventorymgt-bootmvcjpa/src/main/java/com/dxctraining/inventorymgt_mvc/sprint7_item/exception/ItemNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.dxctraining.inventorymgt_mvc.sprint7_item.exception;
-
-public class ItemNotFoundException extends RuntimeException{
-
-		public ItemNotFoundException(String msg) {
-			super(msg);
-		}
-}
