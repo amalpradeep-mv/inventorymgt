b31a5bca6ce04c542b23d0c87a5441dfc9b75ee5
amalpradeep-mv <amalpradeepmv@gmail.com>
2020-08-21 01:20:05 +0530

    update

--- inventory-rest/inventorymgt-bootmvcjpa - server/src/main/java/com/dxctraining/inventorymgt_mvc/controllers/SupplierRestController.java
+++ inventory-rest/inventorymgt-bootmvcjpa - server/src/main/java/com/dxctraining/inventorymgt_mvc/controllers/SupplierRestController.java
@@ -2,7 +2,6 @@ package com.dxctraining.inventorymgt_mvc.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,13 +9,14 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.dxctraining.inventorymgt_mvc.supplier.dto.CreateSupplierRequest;
 import com.dxctraining.inventorymgt_mvc.supplier.dto.UpdateSupplierRequest;
 import com.dxctraining.inventorymgt_mvc.supplier.entities.Supplier;
 import com.dxctraining.inventorymgt_mvc.supplier.service.ISupplierService;
 
-@Controller
+@RestController
 @RequestMapping("/suppliers")
 public class SupplierRestController {
 	
