6673c5f3ae00a9f4b96a57c9a6d95b273b5d7abd
amalpradeep-mv <amalpradeepmv@gmail.com>
2020-08-21 00:16:19 +0530

    update

--- inventorymgt-bootmvcjpa/src/main/java/com/dxctraining/inventorymgt_mvc/sprint6_supplier/exception/SupplierNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.dxctraining.inventorymgt_mvc.sprint6_supplier.exception;
-
-public class SupplierNotFoundException extends RuntimeException{
-
-		public SupplierNotFoundException(String msg) {
-			super(msg);
-		}
-		
-}
